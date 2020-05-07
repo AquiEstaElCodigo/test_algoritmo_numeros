44a65b718ee4b6bb39a6218f12867794fa056ae2
German-RS <german.riveross@gmail.com>
2020-05-07 18:30:49 -0400

    algoritmo_numeros dia dos

--- app/src/main/java/cl/aquilotienes/test_algoritmo_numeros/MainActivity.java
+++ app/src/main/java/cl/aquilotienes/test_algoritmo_numeros/MainActivity.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,13 +21,30 @@ public class MainActivity extends AppCompatActivity {
     TextView tvResultadoNumeros;
     EditText etNumeros;
 
-    int kiñe = 1, epu = 2, küla = 3, meli = 4, kechu = 5, kayu = 6, regle = 7, pura = 8,
-        aylla = 9, mari = 10, pataka = 100, waranga = 1000;
+    Map<Integer, String> numeros;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        numeros = new HashMap<Integer, String>();
+
+        numeros.put(1, "kiñe");
+        numeros.put(2, "epu");
+        numeros.put(3, "küla");
+        numeros.put(4, "meli");
+        numeros.put(5, "kechu");
+        numeros.put(6, "kayu");
+        numeros.put(7, "regle");
+        numeros.put(8, "pura");
+        numeros.put(9, "aylla");
+        numeros.put(10, "mari");
+        numeros.put(100, "pataka");
+        numeros.put(1000, "waranga");
+
+
+
 
         btnNumeros = (Button) findViewById(R.id.btnNumeros);
         etNumeros = (EditText) findViewById(R.id.etNumeros);
@@ -32,37 +54,243 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+
                 int num = Integer.parseInt(etNumeros.getText().toString());
 
-                int cantidad = etNumeros.getText().toString().length();
+                transformarNumeros1(num);
+                //Log.i("NUMERO: ", ""+ transformarNumero(num));
 
-                if(cantidad >=  1 && cantidad < 3)
-                {
-                    Log.i("TIPO","MARI");
-                }
-                else if(cantidad >= 3 && cantidad < 4)
-                {
-                    Log.i("TIPO", "PATAKA");
-                }
-                else if(cantidad >= 4 && cantidad <= 6)
-                {
-                    Log.i("TIPO", "WARANGA");
-                }
 
-                switch(num){
-
-                    case 1:
-                        tvResultadoNumeros.setText("Kiñe");
-                        break;
-                    case 2:
-                        tvResultadoNumeros.setText("Epu");
-                        break;
-                }
 
             }
         });
 
     }
+
+    private String transformarNumero(int num){
+
+        /*
+         *   Algoritmo_numeros Brian Riveros Sepúlveda
+         * */
+
+        String[] basicos =
+                {
+                        "kiñe", "epu", "küla", "meli", "kechu",
+                        "kayu", "regle", "pura", "aylla", "mari"
+                };
+
+        String[] inter =
+                {
+                        "waranga", "pataka", "mari", ""
+                };
+
+        String cadena = Integer.toString(num);
+        String palabra = " ";
+        int var = cadena.length();
+
+
+        for(int i = 0; i <= var; i++){
+
+            if( Character.getNumericValue(cadena.charAt(i)) == 1)
+                palabra = palabra + " " + basicos[0];
+
+            if(Character.getNumericValue(cadena.charAt(i)) == 2)
+                palabra = palabra + " " + basicos[1];
+
+            if(Character.getNumericValue(cadena.charAt(i)) == 3)
+                palabra = palabra + " " + basicos[2];
+
+            if(Character.getNumericValue(cadena.charAt(i)) == 4)
+                palabra = palabra + " " + basicos[3];
+
+            if(Character.getNumericValue(cadena.charAt(i)) == 5)
+                palabra = palabra + " " + basicos[4];
+
+            if(Character.getNumericValue(cadena.charAt(i)) == 6)
+                palabra = palabra + " " + basicos[5];
+
+            if(Character.getNumericValue(cadena.charAt(i)) == 7)
+                palabra = palabra + " " + basicos[6];
+
+            if(Character.getNumericValue(cadena.charAt(i)) == 8)
+                palabra = palabra + " " + basicos[7];
+
+            if(Character.getNumericValue(cadena.charAt(i)) == 9)
+                palabra = palabra + " " + basicos[8];
+
+            if(Character.getNumericValue(cadena.charAt(i)) == 10)
+                palabra = palabra + " " + basicos[9];
+
+            if(var == 6)
+                palabra = palabra + " " + inter[1];
+
+            if(var == 5)
+                palabra = palabra + " " + inter[2];
+
+            if(var == 4)
+                palabra = palabra + " " + inter[0];
+
+            if(var == 3)
+                palabra = palabra + " " + inter[1];
+
+            if(var == 2)
+                palabra = palabra + " " + inter[2];
+
+            if(var == 1)
+                palabra = palabra + " " + inter[3];
+
+            var = var - 1;
+
+        }
+
+        return palabra;
+
+
+    }
+
+
+
+    private void transformarNumeros1(int num){
+
+
+        if(num == 0 && num < 0) // 1-
+        {
+            Log.i("NUMERO: ", "INCORRECTO");
+        }
+        else // 1-
+        {
+            if(num >=1 && num <= 10) //--2
+            {
+                Log.i("NUMERO: ", ""+numeros.get(num));
+            }
+            else //--2
+            {
+                if(num >= 11 && num < 20) //---3
+                {
+
+                    String valor = Integer.toString(num);
+                    char caracter = valor.charAt(1);
+                    int num1 = Character.getNumericValue(caracter);
+
+                    Log.i("NUMEROS: ", " "+ numeros.get(10)+ " " + numeros.get(num1));
+
+
+                }else //---3
+                {
+                    if(num >= 20 && num <= 99) //--4
+                    {
+
+                        String valor = Integer.toString(num);
+                        char dato1 = valor.charAt(0);
+                        char dato2 = valor.charAt(1);
+
+                        int num1 = Character.getNumericValue(dato1);
+                        int num2 = Character.getNumericValue(dato2);
+
+                        if(num2 == 0){
+                            Log.i("NUMERO: ", ""+ numeros.get(num1) +
+                                    " mari ");
+
+                        }else{
+                            Log.i("NUMERO: ", ""+ numeros.get(num1) +
+                                    " mari " + numeros.get(num2));
+                        }
+
+
+                    }
+                    else //--4
+                        {
+
+                            if(num >= 100 && num <= 999)//--5
+                            {
+                                String valor = Integer.toString(num);
+                                char dato1 = valor.charAt(0);
+                                char dato2 = valor.charAt(1);
+                                char dato3 = valor.charAt(2);
+
+                                int num1 = Character.getNumericValue(dato1);
+                                int num2 = Character.getNumericValue(dato2);
+                                int num3 = Character.getNumericValue(dato3);
+
+                                if(num == 100){ //5.1
+
+                                    Log.i("NUMERO: ", ""+ numeros.get(num));
+
+                                }else{ //5.1
+                                    if(num2 == 0 && num3 == 0)//5.1.1
+                                    {
+
+                                        Log.i("NUMERO: ", ""+ numeros.get(num1) +
+                                                " pataka");
+                                    }else //5.1.1
+                                        {
+
+                                        if(num2 == 0) //5.1.1.1
+                                        {
+                                            Log.i("NUMERO: ", "" + numeros.get(num1) +
+                                                    " pataka " + numeros.get(num3));
+                                        }else //5.1.1.1
+                                            {
+                                            Log.i("NUMERO: ", "" + numeros.get(num1) +
+                                                    " pataka " + numeros.get(num2) + " mari "
+                                                    + numeros.get(num3));
+                                        }
+                                    }
+
+                                }
+
+                            }
+                            else //--5
+                                {
+
+                                    if(num >= 1000 && num <= 9999)//--6
+                                    {
+                                        String valor = Integer.toString(num);
+                                        char dato1 = valor.charAt(0);
+                                        char dato2 = valor.charAt(1);
+                                        char dato3 = valor.charAt(2);
+                                        char dato4 = valor.charAt(3);
+
+                                        int num1 = Character.getNumericValue(dato1);
+                                        int num2 = Character.getNumericValue(dato2);
+                                        int num3 = Character.getNumericValue(dato3);
+                                        int num4 = Character.getNumericValue(dato4);
+
+                                        if(num == 1000){
+                                            Log.i("NUMERO: ", ""+ numeros.get(num));
+                                        }
+
+
+
+                                    }
+                                    else //--6
+                                        {
+
+                                    }
+
+
+                            }
+
+
+                    }
+
+
+                }
+
+
+            }
+
+
+        }
+
+
+
+
+
+
+    }
+
+
 
 
 
